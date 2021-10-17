5d17089a4e06970fef7c883c287532569c19bcb6
Scott Tunstall <scott.tunstall@ntlworld.com>
2021-10-17 14:57:04 +0100

    Initial checkin for safekeeping. Runtime: 104 ms, faster than 39.27% of C# online submissions for Valid Number. Memory Usage: 41.9 MB, less than 5.45% of C# online submissions for Valid Number. OK, I wonder why its using so much memory when there's no caching? Weird one. Is it cloning stringsperhaps ?

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace LeetCodeIsValidNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = new Solution();
+            // bool isValid = x.IsNumber("- 123.456e789");
+
+            //Debug.Assert(!x.IsNumber("0e"));
+
+            Debug.Assert(x.IsNumber("005047e+6"));
+
+
+        }
+    }
+}
---
+++ Solution.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeIsValidNumber
+{
+    public class Solution
+    {
+        public bool IsNumber(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return false;
+            
+            return (IsDecimal(s) || IsInteger(s));
+            
+        }
+
+        private bool IsDecimal(string s)
+        {
+            try
+            {
+                var idx = 0;
+                OptionalSign(s, idx, out idx);
+                OptionalSpaces(s, idx, out idx);
+                var digitsForWholePart  = ProcessDigits(s, idx, out idx);
+                MandatoryDot(s, idx, out idx);
+                var digitsForFracPart = ProcessDigits(s, idx, out idx);
+
+                // Guard for string "." where no whole part or fract parts in number!
+                if (digitsForWholePart == 0 && digitsForFracPart == 0)
+                    return false;
+
+                OptionalExponent(s, idx, out idx);
+                EndOfLine(s, idx);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
+
+        private bool IsInteger(string s)
+        {
+            try
+            {
+                var idx = 0;
+                OptionalSign(s, idx, out idx);
+                OptionalSpaces(s, idx, out idx);
+                var digitsForWholePart = ProcessDigits(s, idx, out idx);
+
+                // have we been given an integer with no digits whatsoever?
+                if (digitsForWholePart == 0)
+                    return false;
+
+                OptionalExponent(s, idx, out idx);
+                EndOfLine(s, idx);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
+        private void OptionalSpaces(string s, int idx, out int newIdx)
+        {
+            newIdx = idx;
+
+            char ch = s[newIdx];
+            while (char.IsWhiteSpace(ch) & newIdx < s.Length)
+            {
+                newIdx++;
+                ch = s[newIdx];
+            }
+        }
+
+
+        private void OptionalSign(string s, int idx, out int newIdx)
+        {
+            newIdx = idx;
+
+            char ch = s[newIdx];
+            if (ch == '-' || ch == '+')
+                newIdx ++;
+        }
+
+        
+
+        private void OptionalExponent(string s, int idx, out int newIdx)
+        {
+            newIdx = idx;
+
+            if (idx>= s.Length)
+                return;
+            
+            char ch = s[newIdx];
+            if (ch != 'e' && ch != 'E')
+                return;
+
+            newIdx++;
+
+            if (newIdx >= s.Length)
+                throw new ArgumentException("Expected +, - or digit");
+
+            ch = s[newIdx];
+            if (ch == '+' || ch == '-')
+                newIdx++;
+
+            bool haveAtLeastOneDigit = ProcessDigits(s, newIdx, out newIdx) > 0;
+
+            if (!haveAtLeastOneDigit)
+                throw new ArgumentException("At least one digit must be supplied for exponent.");
+        }
+
+        private int ProcessDigits(string s, int idx, out int newIdx)
+        {
+            newIdx = idx;
+
+            if (idx >= s.Length)
+                return 0;
+
+            int countOfDigits = 0;
+
+            char ch = s[newIdx];
+            while (char.IsDigit(ch))
+            {
+                countOfDigits++;
+
+                if (++newIdx == s.Length)
+                    break;
+
+                ch = s[newIdx];
+            }
+
+            return countOfDigits;
+        }
+
+
+        private void MandatoryDot(string s, int idx, out int newIdx)
+        {
+            newIdx = idx;
+            char ch = s[idx];
+            if (ch != '.')
+                throw new ArgumentException("expected dot");
+
+            newIdx++;
+        }
+
+        private void EndOfLine(string s, int idx)
+        {
+            if (idx < s.Length)
+                throw new ArgumentException($"Have not reached end of line in {s} at position {idx}");
+        }
+
+
+    }
+}
